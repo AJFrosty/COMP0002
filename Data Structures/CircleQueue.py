24f78e6cfff86174206e85be069164e5df0c8467
HeldEnderMen <ajantefraser13@gmail.com>
2023-10-25 10:05:57 -0400

    Updated some Code

--- Data Structures/CircleQueue.py
+++ Data Structures/CircleQueue.py
@@ -1,53 +1,37 @@
 class CircularQueue:
     def __init__(self, max=3):
         self.__max = max
-        self.__queue = [None] * 3
+        self.__queue = [None]*max
         self.__back = -1
         self.__front = 0
         self.__items = 0
     
     def enqueue(self, item):
         if not self.isFull():
-            if self.__back == 2:
+            if self.__back == (self.__max - 1):
                 self.__back = -1
             self.__queue[self.__back + 1] = item
             self.__back += 1
             self.__items += 1
         else:
-            print("Queue is full!")
+            print("Queue is Full")
     
     def dequeue(self):
         if not self.isEmpty():
+            if self.__front == self.__max:
+                self.__front = 0
             toDelete = self.__queue[self.__front]
             self.__queue[self.__front] = None
             self.__front += 1
             self.__items -= 1
             return toDelete
-        return "Queue is empty!"
-
+        return "Queue is Empty"
+    
     def isEmpty(self):
-        return self.__back == -1
+        return self.__items == 0
     
     def isFull(self):
         return self.__items == self.__max
     
     def showQueue(self):
         print(self.__queue)
-
-q = CircularQueue(3)
-q.enqueue(40)
-q.showQueue()
-q.enqueue(10)
-q.showQueue()
-q.enqueue(5)
-q.showQueue()
-q.enqueue(30)
-q.showQueue()
-q.dequeue()
-q.showQueue()
-q.dequeue()
-q.showQueue()
-q.enqueue(33)
-q.showQueue()
-q.dequeue()
-q.showQueue()
